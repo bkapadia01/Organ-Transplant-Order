ad305528e1420291cee6ffd2a7d81047e2d76c22
bkap <bkapadia@ryerson.ca>
2013-11-13 18:16:03 -0500

    trail4ssafdasf

---
+++ trail4/src/trail4.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : trail4.cpp
+// Author      : Bhavin Kapadia
+// Version     :
+// Copyright   : bkapadia
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hewwrrro" << endl; // prints hewwrrro
+	return 0;
+}
